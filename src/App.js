eda4c8641870fadb1fa256818a4e4eb074085839
Vishal Singh Rawat <56162449+vishalsinghrawat29@users.noreply.github.com>
2022-09-03 18:57:11 +0530

    update design

--- src/App.js
+++ src/App.js
@@ -4,44 +4,53 @@ import "./styles.css";
 const TedDB = {
   code: [
     {
-      title: "Don't Just Learn To Code, Learn To Create -Justin Richards",
+      title: "Don't Just Learn To Code, Learn To Create | Justin Richards",
+      link: "https://youtu.be/6rxWc-TNIJI",
       rating: "5/5"
     },
     {
-      title: "Learning to Code is Not Just for Coders -Ali Partovi",
+      title: "Learning to Code is Not Just for Coders | Ali Partovi",
+      link: "https://youtu.be/MvTSPwftvyo",
       rating: "3/5"
     },
     {
-      title: "How I taught myself to code -Litha Soyizwapi ",
+      title: "How I taught myself to code | Litha Soyizwapi ",
+      link: "https://youtu.be/nmZR_JsLCfg",
       rating: "4/5"
     }
   ],
   business: [
     {
-      title: "The Power of an Entrepreneurial Mindset -Bill Roche",
+      title: "The Power of an Entrepreneurial Mindset | Bill Roche",
+      link: "https://youtu.be/Ihs4VFZWwn4",
       rating: "5/5"
     },
     {
-      title: "Entrepreneurship As A State Of Mind -Mr. Ankur Warikoo",
+      title: "Entrepreneurship As A State Of Mind | Mr. Ankur Warikoo",
+      link: "https://youtu.be/dgrwlL82T2I",
       rating: "5/5"
     },
     {
       title:
-        "Zero Equals One: Creating A Business From Nothing -Riley Csernica",
+        "Zero Equals One: Creating A Business From Nothing | Riley Csernica",
+      link: "https://youtu.be/CzJUCxwz8hk",
       rating: "4/5"
     }
   ],
   design: [
     {
-      title: "The first secret of great design -Tony Fadell",
+      title: "The first secret of great design | Tony Fadell",
+      link: "https://youtu.be/9uOMectkCCs",
       rating: "5/5"
     },
     {
-      title: "Design for All 5 Senses -Jinsop Lee",
+      title: "Design for All 5 Senses | Jinsop Lee",
+      link: "https://youtu.be/N6wjC0sxD2o",
       rating: "3/5"
     },
     {
-      title: "The power of design -JD Hooge",
+      title: "The power of design | JD Hooge",
+      link: "https://youtu.be/6rxWc-TNIJI",
       rating: "3/5"
     }
   ]
@@ -57,8 +66,8 @@ export default function App() {
 
   return (
     <div className="App">
-      <h1>🏆 Ted Talks</h1>
-      <p style={{ fontSize: "smaller" }}>
+      <h1 className="head-text">🏆 Ted Talks</h1>
+      <p className="head-sub-text">
         Checkout my favorite Ted Talk. Select a genre to get started
       </p>
       <div>
@@ -87,27 +96,52 @@ export default function App() {
               key={talk.title}
               style={{
                 listStyle: "none",
-                padding: "1rem",
+                padding: "0.8rem",
                 border: "1px solid #D1D5DB",
-                width: "80%",
-                margin: "1rem 0rem",
+                width: "94%",
+                margin: "1.3rem 0rem",
                 borderRadius: "0.5rem"
               }}
             >
-              <div style={{ fontSize: "larger", marginBottom: "0.5rem" }}>
-                {talk.title}
-              </div>
-              <div style={{ fontSize: "medium", color: "#FFB02E" }}>
-                {talk.rating}
+              <div className="title">{talk.title}</div>
+              <div className="ratingText">Rating: {talk.rating}</div>
+              <div>
+                <a href={talk.link} className="watch-btn">
+                  Watch Now
+                </a>
               </div>
             </li>
           ))}
         </ul>
       </div>
-      <footer className="footer">
-        Developed by{" "}
-        <a href="https://vishalsinghrawat.netlify.app/">Vishal Singh Rawat</a>{" "}
-      </footer>
+      <div className="footer-section">
+        <h4>
+          Developed by{" "}
+          <a href="https://vishalsinghrawat.netlify.app/">Vishal Singh Rawat</a>
+        </h4>
+        <ul className="social-links list-non-bullet">
+          <li className="list-item-inline">
+            <a className="link" href="https://github.com/vishalsinghrawat29">
+              Github
+            </a>
+          </li>
+          |
+          <li className="list-item-inline">
+            <a className="link" href="https://twitter.com/vsrawat2906">
+              Twitter{" "}
+            </a>
+          </li>
+          |
+          <li className="list-item-inline">
+            <a
+              className="link"
+              href="https://www.linkedin.com/in/vishalsinghrawat/"
+            >
+              Linkdin
+            </a>
+          </li>
+        </ul>
+      </div>
     </div>
   );
 }
